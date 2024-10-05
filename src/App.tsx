03e35fe7002ba996b5542516b06fc74b3e15a940
Rathish <rathishvbr@gmail.com>
2024-10-05 23:25:50 +0200

    Added shadcn config and button with New York, Slate theme, CSS theme variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Button>Click me</Button>
+    </div>
   )
 }
 
